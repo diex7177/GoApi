5cce5df65f7c559ddfde405cac61fb688596c8bb
Ospina Munoz Diego Andres <diegoo@lagash.com>
2020-03-28 18:59:15 -0500

    init

--- .history/app/users/users_20200328141333.go
+++
@@ -1,8 +0,0 @@
-package users
-
-type User struct {
-	Id      int    'json:"id"'
-	Name    string 'json:"name,omitempty"'
-	SurName string 'json:"surname,omitempty"'
-	Age     byte   'json:"age,omitempty"'
-}
